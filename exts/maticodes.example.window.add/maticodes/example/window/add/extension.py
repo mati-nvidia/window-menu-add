946ad4970b77456a9c1426c19ea3b254d5b15514
mati-nvidia <mcodesal@nvidia.com>
2023-05-10 12:42:52 -0400

    Multiwindow example

--- exts/maticodes.example.window.add/maticodes/example/window/add/extension.py
+++ exts/maticodes.example.window.add/maticodes/example/window/add/extension.py
@@ -1,3 +1,5 @@
+from functools import partial
+
 import carb
 import omni.ext
 import omni.kit.ui
@@ -10,25 +12,33 @@ class WindowMenuAddExtension(omni.ext.IExt):
         carb.log_info("[maticodes.example.window.add] WindowMenuAddExtension startup")
 
         # Note the "Window" part of the path that directs the new menu item to the "Window" menu.
-        self._menu_path = f"Window/{WINDOW_TITLE}"
-        self._window = None
-        self._menu = omni.kit.ui.get_editor_menu().add_item(self._menu_path, self._on_menu_click, True)
+        self._windows = {
+            "My First Window": None,
+            "My Second Window": None
+        }
+        self._menus = []
+        for window_title in self._windows:
+            menu = omni.kit.ui.get_editor_menu().add_item(f"Window/{window_title}", partial(self._on_menu_click, window_title), True)
+            self._menus.append(menu)
 
     def on_shutdown(self):
         carb.log_info("[maticodes.example.window.add] WindowMenuAddExtension shutdown")
 
-        omni.kit.ui.get_editor_menu().remove_item(self._menu)
-        if self._window is not None:
-            self._window.destroy()
-            self._window = None
+        for menu in self._menus:
+            omni.kit.ui.get_editor_menu().remove_item(menu)
+        for window in self._windows:
+            if window is not None:
+                window.destroy()
+        self._windows = None
+        self._menus = None
 
-    def _on_menu_click(self, menu, toggled):
+    def _on_menu_click(self, window_title, menu, toggled):
         """Handles showing and hiding the window from the 'Windows' menu."""
         if toggled:
-            if self._window is None:
-                self._window = MyCustomWindow(WINDOW_TITLE, self._menu_path)
+            if self._windows[window_title] is None:
+                self._windows[window_title] = MyCustomWindow(window_title, menu)
             else:
-                self._window.show()
+                self._windows[window_title].show()
         else:
-            if self._window is not None:
-                self._window.hide()
+            if self._windows[window_title] is not None:
+                self._windows[window_title].hide()
